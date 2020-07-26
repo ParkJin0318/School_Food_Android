9aff99cd502180e00e8f4c8655f3992ec248aa48
박진 <50200481+flaw5886@users.noreply.github.com>
2020-07-26 19:02:40 +0900

    [ADD] iOS Project

---
+++ School_Food_iOS/School_Food/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  School_Food
+//
+//  Created by 박진 on 2020/07/26.
+//  Copyright © 2020 com.meals.school_food. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
